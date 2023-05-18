f17112b648031bb564df5e8adc20e57c33fa1565
lurnfx <bhushanjawale80@gmail.com>
2023-05-18 11:40:03 +0530

    Enquiry class Update

--- HomeAppLoanApplication/src/main/java/com/homeloan/main/controller/Address.java
+++ HomeAppLoanApplication/src/main/java/com/homeloan/main/controller/Address.java
@@ -4,11 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name = "cust_enquiry")
+@Data
 public class Address {
     @Id
 	private Integer enquiryId;
+    private String fullName;
 	
 	
 }
