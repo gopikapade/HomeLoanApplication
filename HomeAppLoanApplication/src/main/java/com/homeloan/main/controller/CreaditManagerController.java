e34896d0778690263aa887c0c2e417af56fa67d8
gopikapade <kapadegopi.gk2022@gmail.com>
2023-05-20 11:33:18 +0530

    Test 20-05-2023

--- HomeAppLoanApplication/src/main/java/com/homeloan/main/controller/CreaditManagerController.java
+++ HomeAppLoanApplication/src/main/java/com/homeloan/main/controller/CreaditManagerController.java
@@ -3,9 +3,7 @@ package com.homeloan.main.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
