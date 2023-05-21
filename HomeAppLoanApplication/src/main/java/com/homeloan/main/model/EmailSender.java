22db21790731208eb3b97a505aca747de6d5c61a
gopikapade <kapadegopi.gk2022@gmail.com>
2023-05-21 14:23:57 +0530

    Email Sender Backend
    Email By Gopi

---
+++ HomeAppLoanApplication/src/main/java/com/homeloan/main/model/EmailSender.java
@@ -0,0 +1,18 @@
+package com.homeloan.main.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailSender {
+	
+	
+	private String to,from,subject,textBodyMsg;
+	
+	
+	
+
+}
