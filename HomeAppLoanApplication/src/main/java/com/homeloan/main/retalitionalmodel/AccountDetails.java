9d4d7f98d391befb678cdd863cd3e5db359acd5e
gopikapade <kapadegopi.gk2022@gmail.com>
2023-05-27 01:05:31 +0530

    Revert "Revert "Ifsc And Bbank Name Account""
    This reverts commit fab3b4306339d9affe060689c0f290d28d6309d9.

--- HomeAppLoanApplication/src/main/java/com/homeloan/main/retalitionalmodel/AccountDetails.java
+++ HomeAppLoanApplication/src/main/java/com/homeloan/main/retalitionalmodel/AccountDetails.java
@@ -19,7 +19,7 @@ public class AccountDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer accountId;
-	private String accountType,
+	private String accountType,ifsc,bankname,	
 
 	accountHolderName,
 	accountStatus;
