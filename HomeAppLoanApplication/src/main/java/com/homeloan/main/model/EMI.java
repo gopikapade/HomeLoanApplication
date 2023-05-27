09cdea37b943647218e474447707399d21588a5b
Bhushan <80579471+lurnfx@users.noreply.github.com>
2023-05-27 13:54:14 +0530

    Changes In Branch MErge

--- HomeAppLoanApplication/src/main/java/com/homeloan/main/model/EMI.java
+++ HomeAppLoanApplication/src/main/java/com/homeloan/main/model/EMI.java
@@ -10,19 +10,27 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class EMI {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	private Double LoanAmmount;
 	private Integer tenure;
 	private Double intrestrate;
 	private Double monthlyEmi;
 	private Date date = new Date();
+<<<<<<< HEAD
 
+=======
+	
+	
+	
+>>>>>>> 5d389858b427cdf44d9b6de5ba73e7057bf7f96a
 }
